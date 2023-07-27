326a4e8381b8b41fcc42a7ef08b2b4a0e72801c9
elimut <121231529+elimut@users.noreply.github.com>
2023-07-27 18:10:10 +0200

    methode recup logged

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,6 @@
+import { isPlatformBrowser } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, delay, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,13 @@ export class AuthService {
   // si user demande à accèder à la liste des pokemons ou autre, et qu'il n'a pas le droit demande de co, et si co ok redir au bon endroit sur la page
 
   login(name: string, password: string): Observable<boolean> {
-
+    const isLoggedIn = (name == "pikachu" && password =='pikachu');
+    // recup rst pour savoir si user auth ou non, puis délègue dans le tps d'une scd et ensuite mise à jour ppt isLoggedIn
+    return of(isLoggedIn).pipe
+      (delay(1000),
+      tap(isLoggedIn => this.isLoggedIn = isLoggedIn)
+    // simuler délai serveur
+      );
   }
 
   logout() {
